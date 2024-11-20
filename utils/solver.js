620c7745a6c783db01bf7530b043eedbaa1a41dd
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-20 11:43:33 +0800

    Update solver.js

--- utils/solver.js
+++ utils/solver.js
@@ -2,7 +2,7 @@ import { Solver } from "@2captcha/captcha-solver";
 let key;
 export async function solveCaptcha(key) {
     const solver = new Solver(key);
-    const pageurl = "https://app.megafin.xyz/upgrade?ref=a329efba";
+    const pageurl = "https://app.megafin.xyz/upgrade?ref=919d0976";
     const sitekey = "0x4AAAAAAA0SGzxWuGl6kriB";
     try {
         const result = await solver.cloudflareTurnstile({ pageurl, sitekey });
